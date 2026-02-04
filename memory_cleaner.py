a03222c352f18932cfd4b880eef5c239f27295a6
Ryan Meline <ryan.c.meline@gmail.com>
2026-02-03 19:11:35 -0800

    Chapter and Status parsing completed

--- memory_cleaner.py
+++ memory_cleaner.py
@@ -37,8 +37,6 @@ def name_parse(name: str):
         except ValueError:
             val = ""
         
-       #  if template.name.strip().lower() == "tooltip":
-      #  wikicode.replace(template, val)
         if template.name.strip().lower() == "c":
             val = " | " + val # for formatting / style lol. all the {{c| were bonus info, so separating them with a |
         wikicode.replace(template,val)
@@ -51,14 +49,51 @@ def chapter_parse(chapter: str):
     # templace shananigans, can probably copy a lot from the names
 
     # can copy a lot (most) code from name_parse
+    chapter_split = []
+    if "|" in chapter and "id=" in chapter or "data-sort-value=" in chapter:
+        chapter_split = chapter.split("|", 1)
+        chapter = " "
+        if "id=" in chapter_split[0] or "data-sort-value=" in chapter_split[0]:
+            chapter_split[0] = " "
 
-    return chapter
+    chapter = chapter.replace("<br>", " | ")
+
+    wikicode = mw.parse(chapter)
+    for template in reversed(wikicode.filter_templates(recursive=True)): #undo from the inside out
+        try:
+            val = " " + str(template.get(1).value).strip()
+        except ValueError:
+            val = " "
+        
+        if template.name.strip().lower() == "c":
+            val = " | " + val # for formatting / style lol. all the {{c| were bonus info, so separating them with a |
+        wikicode.replace(template,val)
+
+
+    return wikicode.strip_code().strip() 
 
 def status_parse(status: str):
-    return status
+    status = re.sub("<(.*?)>", " ", status)
+#    status = BeautifulSoup(status, "html.parser").get_text()
+
+    wikicode = mw.parse(status)
+    for template in reversed(wikicode.filter_templates(recursive=True)): #undo from the inside out
+        try:
+            val = " " + str(template.get(1).value).strip()
+        except ValueError:
+            val = " "
+        
+        if template.name.strip().lower() == "c":
+            val = "|" + val # for formatting / style lol. all the {{c| were bonus info, so separating them with a |
+        wikicode.replace(template,val)
+
+
+    return wikicode.strip()
 
 def desc_parse(desc: str):
-    return desc
+    desc = re.sub("<(.*?)>", " ", desc)
+
+    return desc.strip()
 
 
 #def main():
@@ -71,7 +106,7 @@ with open(INPUT_FILE, "r", encoding="utf-8") as f:
 all_pages = []
 
 # set to ademndum mode, change to write once json structures are built (in ademndum bc adding in a loop, can add json all at once)
-with open("cleaned_memory_names.json", "w") as f:
+with open("cleaned_memory_names.json", "w", encoding="utf-8") as f:
 #     print("test")
     for page in data:
         for page_name, items in page.items():
@@ -82,17 +117,14 @@ with open("cleaned_memory_names.json", "w") as f:
                 # can fix by making new json objects
                 # not doing that rn
                 if "Chapter Received" in item:
-                    print(item["Chapter Received"])
-                    item["Chapter Received"] = " " #chapter_parse(item["Chapter Received"])
+                    item["Chapter Received"] = chapter_parse(item["Chapter Received"])
                 elif "Chapter Appeared" in item:
-                    print(item["Chapter Appeared"])
-                    item["Chapter Appeared"] = " " #chapter_parse(item["Chapter Appearered"])    
+                    item["Chapter Appeared"] = chapter_parse(item["Chapter Appeared"])    
                 else:
-                    print(item["Chapter"])
-                    item["Chapter"] = " " #chapter_parse(item["Chapter"])
+                    item["Chapter"] = chapter_parse(item["Chapter"])
 
-                item["Status"] = " " #status_parse(item["Status"])
-                item["Description"] = " " #desc_parse(item["Description"])
+                item["Status"] = status_parse(item["Status"])
+                item["Description"] = desc_parse(item["Description"])
     json.dump(data, f, ensure_ascii=False, indent = 2)
     #f.write(data)
  #   else:
